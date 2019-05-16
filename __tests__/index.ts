d0963814b3d7ca0d74fc8fd004abfcac33c09f49
Louise Bicker Caarten <louisehmbc@gmail.com>
2019-05-16 10:13:11 -0400

    Add tests

---
+++ __tests__/index.ts
@@ -0,0 +1,55 @@
+import { add, subtract, multiply, divide } from "../index";
+
+describe("Stupid calculator", () => {
+
+  describe("addition", () => {
+    test("0 + 0", () => {
+      expect(add(0, 0)).toBe(0);
+    });
+    test("2 + 3", () => {
+      expect(add(2, 3)).toBe(5);
+    });
+    test("2 + -3", () => {
+      expect(add(2, -3)).toBe(-1);
+    });
+    test("-2 + 3", () => {
+      expect(add(-2, 3)).toBe(1);
+    })
+    test("-2 + -3", () => {
+      expect(add(-2, -3)).toBe(-5);
+    });
+  });
+
+    test("0 - 0", () => {
+      expect(subtract(0, 0)).toBe(0);
+    })
+    test("3 - 5", () => {
+      expect(subtract(3, 5)).toBe(-2);
+    });
+    test("5 - 3", () => {
+      expect(subtract(5, 3)).toBe(2);
+    });
+    test("5 - -3", () => {
+      expect(subtract(5, -3)).toBe(8);
+    });
+    test("-5 - 3", () => {
+      expect(subtract(-5, 3)).toBe(-8);
+    })
+    test("-5 - -3", () => {
+      expect(subtract(-5, -3)).toBe(-2);
+    });
+  });
+
+  describe("multiplication", () => {
+
+  });
+
+  describe("division", () => {
+    test('10 / 0', () => {
+      expect(divide(10, 0)).toThrowError();
+    });
+    test('0 / 10', () => {
+      expect(divide(0, 10)).toBe(0);
+    });
+  });
+});
